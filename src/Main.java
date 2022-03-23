73765f654a37909665eb586f0081a3e3290bbfb2
joanmadrigalr <98435545+joanmadrigalr@users.noreply.github.com>
2022-03-22 18:24:10 -0600

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
 
@@ -13,6 +14,8 @@ public class Main {
 
         Board boardPerson = new Board();
         Board boardCPU = new Board();
+                
+        
 
         Person person1 = new Person(playerName, boardPerson);
         CPU cpu1 = new CPU(CPUName, boardCPU);
@@ -25,12 +28,49 @@ public class Main {
         if (option == 1 ) {
 
         } else if (option == 2) {
-
+            
         } else if (option==3){
             boardPerson.showMatrix();
-        }
-        else {
+        
+        } else {
             System.out.println("Invalid Option");
+              
         }
+           
+        Scanner sn = new Scanner(System.in);
+        boolean exit = false;
+        int opt; //We will save the user's option
+        do{
+            System.out.println("1. Attack");
+            System.out.println("2. See attack board");
+            System.out.println("3. See own dashboard");
+            System.out.println("4. Exit");
+            try {
+                System.out.println("Enter one of the options");
+                opt = sn.nextInt();
+                switch (opt) {
+                    case 1:
+                        System.out.println("You selected Attack");
+                        
+                        break;
+                    case 2:
+                        System.out.println("You selected View Attack Board");
+                        break;
+                    case 3:
+                        System.out.println("You have selected View Own Dashboard");
+                        break;
+                    case 4:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Only numbers between 1 and 4");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("You must insert a number");
+                sn.next();
+            }
+        }
+        while (!exit) ;
     }
-}
+}        
+   
