7c57264a7e567dc920ed26aeb1222989496773ca
jocelynechavarria <josy_c95@hotmail.cm>
2022-03-23 15:39:09 -0600

    Code with the 2 menus and board

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-
+import javax.swing.JOptionPane;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -17,20 +18,60 @@ public class Main {
         Person person1 = new Person(playerName, boardPerson);
         CPU cpu1 = new CPU(CPUName, boardCPU);
 
-        System.out.println("Hi " + person1.getName() + ", the CPU name assigned is " + cpu1.getName() +", please select the correct option: ");
-        System.out.println("1. Assign coordinates");
-        System.out.println("2. Assign random");
+        //this is the menu to assign ships
+        System.out.println("Hi " + person1.getName() + ", the name assigned to the CPU is " + cpu1.getName() + ", please select one of the 2 options to assign your ships: ");
+        System.out.println("1. Assign by coordinates");
+        System.out.println("2. Assign randomly");
         System.out.println("3. Print Matrix");
+
+        //task to assign ships
         int option = input.nextInt();
-        if (option == 1 ) {
+        if (option == 1) {
 
         } else if (option == 2) {
 
-        } else if (option==3){
+        } else if (option == 3) {
             boardPerson.showMatrix();
-        }
-        else {
+        } else {
             System.out.println("Invalid Option");
         }
+        System.out.println(person1.getName() + "now that the ships are assigned please select one of the options to start playing");
+
+        Scanner sn = new Scanner(System.in);
+        boolean exit = false;
+        int opt; //We will save the user's option
+        do {
+            System.out.println("1. Attack");
+            System.out.println("2. See attack board");
+            System.out.println("3. See own dashboard");
+            System.out.println("4. Exit");
+            try {
+                System.out.println("Enter one of the options");
+                opt = sn.nextInt();
+                switch (opt) {
+                    case 1:
+                        System.out.println("You selected Attack");
+
+                        break;
+                    case 2:
+                        System.out.println("You selected View Attack Board");
+                        break;
+                    case 3:
+                        System.out.println("You have selected View Own Dashboard");
+                        break;
+                    case 4:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Only numbers between 1 and 4");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("You must insert a number");
+                sn.next();
+            }
+        }
+        while (!exit);
     }
 }
+
+
